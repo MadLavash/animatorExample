a237cd9046af0fb853424386dcae1e6f0e82133c
Evgeniy <rodikove@gmail.com>
2017-11-29 21:25:01 +0300

    Initial commit for animatorExample project

---
+++ Assets/Scenes/testZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testZone : MonoBehaviour {
+
+    [Range(-100f, 100f)]
+    public float positionX;
+    [Range(-100f, 100f)]
+    public float positionY;
+    [SerializeField]
+    private RectTransform testRect;
+    private Vector2 anchoredPos;
+
+    private void Update()
+    {
+        SetPosition();
+    }
+
+    void SetPosition()
+    {
+        testRect.anchoredPosition = new Vector2(positionX, positionY);
+    }
+
+
+}
